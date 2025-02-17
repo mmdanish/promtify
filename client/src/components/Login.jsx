160c5be3f74afd9593c9017e5d2339b313673f6e
mmdanish <mhddanish567@gmail.com>
2025-02-17 14:09:10 +0530

    commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import userIcon from "../assets/user_icon.svg";
 import { AppContext } from "../context/AppContext";
+import { motion } from "motion/react";
 
 const Login = () => {
   const [state, setState] = useState("Login");
@@ -16,8 +17,13 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-12 rounded-xl text-slate-500">
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <motion.form
+      initial={{ opacity: 0.2, y: 50 }}
+      transition={{ duration: 0.3 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="relative bg-white p-12 rounded-xl text-slate-500">
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -91,7 +97,7 @@ const Login = () => {
           alt=""
           className="absolute top-5 right-5 cursor-pointer"
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
