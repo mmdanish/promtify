70b8f3b199856b037186c522a03ef5d46e3b15fa
mmdanish <mhddanish567@gmail.com>
2025-03-26 10:25:39 +0530

    commit

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,18 +3,28 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const [user, setUser] = useState(null);
-    const [showLogin, setShowLogin] = useState(false);
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [credit, setCredit] = useState(false);
 
-    const value = {
-        user, setUser, showLogin, setShowLogin
-    }
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    token,
+    setToken,
+    credit,
+    setCredit,
+  };
 
-export default AppContextProvider
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
